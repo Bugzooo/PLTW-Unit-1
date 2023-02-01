a2c02ffeea1baae7cc53babc7e9f06c0f424e2ef
Bugzooo <124206568+Bugzooo@users.noreply.github.com>
2023-02-01 14:03:27 -0800

    Add files via upload

---
+++ 1-Turtle.py
@@ -0,0 +1,33 @@
+import turtle as trtl
+
+painter = trtl.Turtle()
+
+painter.pencolor("red")
+painter.forward(100)
+painter.right(90)
+painter.pencolor("blue")
+painter.circle(-100)
+painter.circle(-50)
+painter.pencolor("red")
+painter.forward(100)
+painter.right(90)
+painter.pencolor("blue")
+painter.circle(-100)
+painter.circle(-50)
+painter.pencolor("red")
+painter.forward(100)
+painter.right(90)
+painter.pencolor("blue")
+painter.circle(-100)
+painter.circle(-50)
+painter.pencolor("red")
+painter.forward(100)
+painter.right(90)
+painter.pencolor("blue")
+painter.circle(-100)
+painter.circle(-50)
+painter.pencolor("red")
+
+ 
+wn = trtl.Screen()
+wn.mainloop()
