80d7c93f1c678c49c17efabadeb4a923d87f2765
Bugzooo <124206568+Bugzooo@users.noreply.github.com>
2023-02-02 12:33:29 -0800

    Updated Slightly, 1-Turtle.py

--- 1-Turtle.py
+++ 1-Turtle.py
@@ -2,6 +2,7 @@ import turtle as trtl
 
 painter = trtl.Turtle()
 
+print("COMPREHENDED, WRITTEN, AND FINALIZED BY DAVID")
 painter.pencolor("red")
 painter.forward(100)
 painter.right(90)
@@ -27,6 +28,7 @@ painter.pencolor("blue")
 painter.circle(-100)
 painter.circle(-50)
 painter.pencolor("red")
+##COMPREHENDED, WRITTEN, AND FINALIZED BY DAVID
 
  
 wn = trtl.Screen()
